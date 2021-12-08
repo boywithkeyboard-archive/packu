f6fb4a8a2ceae62f886137ed2181f458287cd303
Azury <azury.wtf@gmail.com>
2021-12-08 13:56:55 +0100

    added build script

---
+++ build.js
@@ -0,0 +1,10 @@
+const esbuild = require('esbuild')
+
+esbuild.build({
+  entryPoints: ['src/index.ts'],
+  bundle: true,
+  minify: true,
+  platform: 'node',
+  external: ['esbuild'],
+  outfile: 'bin/index.js'
+})
