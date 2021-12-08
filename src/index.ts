4aa58a6f8d01687a9ce5b04152f73d2e965556a3
Azury <azury.wtf@gmail.com>
2021-12-08 13:57:39 +0100

    edited main file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,53 @@
-import esbuild from 'esbuild'
-import cssModules from 'esbuild-css-modules-plugin'
-import { optimize } from 'svgo'
-import { minify } from 'terser'
-import { minify as minifyHTML } from 'html-minifier-terser'
+#!/usr/bin/env node
+
+import { hideBin } from 'yargs/helpers'
 import yargs from 'yargs'
-import boxen from 'boxen'
-import chalk from 'chalk'
+import bundle from './modes/bundle'
+import minify from './modes/minify'
+import zip from './modes/zip'
+
+yargs(hideBin(process.argv))
+  .command('*', 'Bundle a specific file.', () => {}, (args) => bundle({
+    directory: process.cwd(),
+    input: args.input ?? 'src/index.js',
+    output: args.input ?? 'build/index.js',
+    watch: args.watch ?? false,
+    esm: args.esm ?? false,
+    css: args.css ?? false
+  }))
+  .command('minify', 'fetch the contents of the URL', () => {}, (args) => minify({
+    directory: process.cwd(),
+    input: args.input,
+    output: args.input
+  }))
+  .command('zip', 'fetch the contents of the URL', () => {}, (args) => zip({
+    directory: process.cwd(),
+    input: args.input,
+    output: args.input
+  }))
+  .option('css', {
+    type: 'boolean',
+    description: 'Enable CSS modules.'
+  })
+  .option('watch', {
+    alias: 'w',
+    type: 'boolean',
+    description: 'Enable bundling in watch mode.'
+  })
+  .option('input', {
+    alias: 'i',
+    type: 'string',
+    description: 'Define the input file.'
+  })
+  .option('output', {
+    alias: 'o',
+    type: 'string',
+    description: 'Define the output file.'
+  })
+  .option('esm', {
+    type: 'boolean',
+    description: 'Set JavaScript output standard to ESM.'
+  })
+  .demandCommand(1)
+  .parse()
 
