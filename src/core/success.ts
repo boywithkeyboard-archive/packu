b14d03fa1dda3ae39695d2cffa77b028ec33c67e
Azury <azury.wtf@gmail.com>
2021-12-09 20:34:44 +0100

    edited success handler

--- src/core/success.ts
+++ src/core/success.ts
@@ -1,6 +1,5 @@
-import boxen from 'boxen'
 import chalk from 'chalk'
 
 export default (time: number, message?: string) => {
-  console.log(boxen(`${chalk.bold.greenBright(message ?? 'BUNDLED PACKAGE!')}\n${chalk.blackBright(`within ~${time}s`)}`, { title: chalk.bold.yellow('BUNDLER'), titleAlignment: 'center', padding: 1, textAlignment: 'center', margin: 2, borderStyle: 'round', borderColor: 'yellow' }))
+  chalk.bold.greenBright(message ?? 'BUNDLED PACKAGE! ') + chalk.blackBright(`within ~${time}s`)
 }
