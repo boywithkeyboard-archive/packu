fb54458a48c519cf92bffb17175a6c2225026145
Azury <azury.wtf@gmail.com>
2021-12-09 20:34:29 +0100

    edited watch script

--- watch.js
+++ watch.js
@@ -5,6 +5,6 @@ esbuild.build({
   bundle: true,
   watch: true,
   platform: 'node',
-  external: ['esbuild', 'fs', 'terser', 'html-minifier-terser'],
-  outfile: 'bin/index.js'
+  external: ['esbuild', 'fs', 'terser', 'html-minifier-terser', 'archiver', 'chalk', 'css-tree', 'esbuild-css-modules-plugin', 'fs-extra', 'glob', 'svgo', 'yargs'],
+  outfile: 'build/index.js'
 })
