85049a30601233041d709b89843b965e0a54ac45
Azury <azury.wtf@gmail.com>
2021-12-23 18:07:14 +0100

    edited success module

--- src/core/success.ts
+++ src/core/success.ts
@@ -1,5 +1,37 @@
 import chalk from 'chalk'
+import boxen from 'boxen'
 
-export default (time: number, message?: string) => {
-  chalk.bold.greenBright(message ?? 'BUNDLED PACKAGE! ') + chalk.blackBright(`within ~${time}s`)
+const messages = [
+  'WHAT A MASTERPIECE!',
+  'WELL DONE!',
+  'GOOD JOB!',
+  'NOT BAD!',
+  'GG!',
+  'NICE!',
+  'YAYYY!'
+]
+
+const success = ({ title, message }: {
+  title?: string,
+  message: string
+}) => {
+  title = chalk.bold.greenBright(title ?? messages[Math.floor(Math.random() * messages.length)])
+  message = chalk.blackBright(message)
+
+  console.log(boxen(`${title} \n ${message}`, {
+    padding: 1,
+    borderStyle: 'round',
+    margin: {
+      top: 10,
+      right: 6,
+      bottom: 3,
+      left: 6
+    },
+    textAlignment: 'center',
+    borderColor: 'yellow',
+    title: '📦 packu',
+    titleAlignment: 'center'
+  }))
 }
+
+export default success
