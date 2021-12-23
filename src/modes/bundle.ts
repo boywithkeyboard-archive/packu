38a8c1a39130f894e498567a45ab420f9b9e26d1
Azury <azury.wtf@gmail.com>
2021-12-23 18:07:37 +0100

    added message to watch mode

--- src/modes/bundle.ts
+++ src/modes/bundle.ts
@@ -1,44 +1,50 @@
-import esbuild from 'esbuild'
+import * as esbuild from 'esbuild'
 import cssModules from 'esbuild-css-modules-plugin'
-import success from '../core/success'
 import { minify } from 'terser'
-import error from '../core/error'
 import { readFile, writeFile } from 'fs/promises'
+import error from '../core/error'
+import success from '../core/success'
+import notice from '../core/notice'
 
-export default (config: any) => {
+export default (configuration: any) => {
   try {
     const start = Date.now()
 
-    const { directory, input, output, watch, esm, css, node, exclude } = config
+    const { directory, input, output, watch, esm, css, node, exclude } = configuration
 
-    const build = async () => {
+    const bundlePackage = async () => {
       await esbuild.build({
         entryPoints: [directory + input],
         bundle: true,
         ...(esm && { format: 'esm', }),
-        ...(watch && { watch: true, }),
+        ...(watch && { watch: {
+          onRebuild(err) {
+            if (err) error({ message: `Rebuild failed at ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}` })
+            else success({ message: `Rebuilt package at ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}` })
+          }
+        }, }),
         outfile: directory + output,
         ...(css && { plugins: [cssModules()] }),
         ...(exclude && { external: exclude }),
         ...(node && { platform: 'node' })
       })
 
+      if (watch) notice({ title: '👀 WATCHING!', message: 'Compiling in Watch Mode...' })
+
       if (!watch) {
         const file = await readFile(directory + output, 'utf8')
-        const result = await minify(file, {
+        const result: any = await minify(file, {
           format: {
             comments: false
           }
         })
         await writeFile(directory + output, result.code, 'utf-8')
+        const stop = Date.now()
+        success({ message: `Built package within ~${(stop - start) / 1000}s` })
       }
     }
-    build()
-  
-    const stop = Date.now()
-  
-    success((stop - start) / 1000)
+    bundlePackage()
   } catch (err) {
-    error(err)
+    error({ message: `Building failed at ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}` })
   }
 }
