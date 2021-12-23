e8e0e9d1fb81d8b5d45e1c9b5faf7625a95ecf65
Azury <azury.wtf@gmail.com>
2021-12-23 18:06:58 +0100

    edited error module

--- src/core/error.ts
+++ src/core/error.ts
@@ -1,5 +1,35 @@
 import chalk from 'chalk'
+import boxen from 'boxen'
 
-export default (err: any) => {
-  console.log(chalk.bold.redBright('FAILED TO BUNDLE PACKAGE! ') + chalk.blackBright(err.toString()))
+const messages = [
+  'WHAT A FAIL!',
+  'WHAT HAVE YOU DONE?!',
+  'HELL NO!',
+  'NOOOOOO!',
+  'OH NO!'
+]
+
+const error = ({ title, message }: {
+  title?: string,
+  message?: string
+}) => {
+  title = chalk.bold.redBright(title ?? messages[Math.floor(Math.random() * messages.length)])
+  message = chalk.blackBright(message)
+
+  console.log(boxen(`${title} \n ${message}`, {
+    padding: 1,
+    borderStyle: 'round',
+    margin: {
+      top: 10,
+      right: 6,
+      bottom: 3,
+      left: 6
+    },
+    textAlignment: 'center',
+    borderColor: 'yellow',
+    title: '📦 packu',
+    titleAlignment: 'center'
+  }))
 }
+
+export default error
